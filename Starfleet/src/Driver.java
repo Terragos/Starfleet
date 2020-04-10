5b95e46685af058ec6a5e39b247900bfe1fa1b4b
Office <Office@192.168.254.84>
2020-04-09 20:04:04 -0400

    hey

--- Starfleet/src/Driver.java
+++ Starfleet/src/Driver.java
@@ -1,38 +1,109 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Driver {
 
-	/* Method */
-	private static void printNums(int num) {
+	/* MAIN Method */
+	public static void main(String[] args) 
+	{
+		int numImpulses=0;
 		
-		/* Declaring a Variable */
-		int x;
+		// Die Roll is equal to the die roll that was printed.
+		// Do not need to use but we do have access to it here.
+		// int dieRoll = rollDice(1, 36, true);
 		
-		/* Initializing a Variable */
-		x = num;
-		// int x = num 
-		// is also valid
-		
-		/* While Loop */
-		while(x >= 0) {
-			
-			/* Print Statement */
-			System.out.println("X: " + x);
-			x--;
-			
-			/* If Statement */
-			if(x > 10) {
-				System.out.println("DAAAAAANG thats a big number.");
-			}
-			else {
-				// Do Nothing Currently
-			}
-			
+		/* Making an Object of type Starship */
+		StarShip s1 = new StarShip();
+		s1.name = "FED-CV";
+		s1.speed = 2;
+
+		StarShip s2 = new StarShip();
+		s2.name = "KLI-D7";
+		s2.speed = 5;
+
+		StarShip s3 = new StarShip();
+		s3.name = "GOR-CA";
+		s3.speed = 24;
+
+		if (s1.speed>numImpulses) {
+			numImpulses = s1.speed;
+		}
+
+		if (s2.speed>numImpulses) {
+			numImpulses = s2.speed;
+		}
+
+		if (s3.speed>numImpulses) {
+			numImpulses = s3.speed;
 		}
 		
-	}
+		s1.spi = (double)s1.speed / numImpulses;
+		s2.spi = (double)s2.speed / numImpulses;
+		s3.spi = (double)s3.speed / numImpulses;
+
+		System.out.println("================================================");
+		System.out.println("Ship Speeds");
+		System.out.println(s1.name + " = " + s1.speed + "      " + s2.name + " = " + s2.speed + "      "+ s3.name + " = " + s3.speed);
+		System.out.println("================================================");
+		System.out.println();
+
+		for(int i = 1; i<=numImpulses; i++)
+		{
+			if (i < 10) {
+			System.out.print("Impulse 0" + i + ":");
+			} else {
+				System.out.print("Impulse " + i + ":");
+			}
+
+			s1.distrv = ((s1.distrv * 10) + (s1.spi * 10)) / 10;
+			s2.distrv = ((s2.distrv * 10) + (s2.spi * 10)) / 10;
+			s3.distrv = ((s3.distrv * 10) + (s3.spi * 10)) / 10;
+		
+			int x1 = (int)s1.distrv;
+		
+			if ((((s1.distrv * 10) - (s1.spi * 10)) / 10) < x1) {
+				System.out.print("   " + s1.name);
+				} else {
+					System.out.print("         ");
+			}
+		
+			int x2 = (int)s2.distrv;
+		
+			if ((((s2.distrv * 10) - (s2.spi * 10)) / 10) < x2) {
+				System.out.print("    " + s2.name);
+				} else {
+					System.out.print("          ");
+			}
+		
+			int x3 = (int)s3.distrv;
+		
+			if ((((s3.distrv * 10) - (s3.spi * 10)) / 10) < x3) {
+				System.out.print("    " + s3.name);
+				} else {
+					System.out.print("          ");
+			}
 	
-	public static int rollDice(int numOfDice, int numOfSides, boolean print){
+			System.out.println();
+			System.out.println();
+	
+			//  Need "WAIT FOR SPACEBAR" CODE
+			//  that does NOT type letters on the screen
+			//  or  show hard returns (causes skipped lines)
+			
+			
+			// Scanner console = new Scanner(System.in);
+			// String guess = console.next();
+			
+			//  try {
+			//  	System.in.read();
+			//  } catch (Exception e){}
+		
+		}  
+	}
+
+	/* rollDice */
+	public static int rollDice(int numOfDice, int numOfSides, boolean print)
+	{
 		Random randomGenerator = new Random();
 		int dieRoll;
 		int totalDieRoll = 0;
@@ -60,26 +131,6 @@ public class Driver {
 		return totalDieRoll;
 	}
 	
-	/* MAIN Method */
-	public static void main(String[] args) {
-		
-		// Die Roll is equal to the die roll that was printed.
-		// Do not need to use but we do have access to it here.
-		int dieRoll = rollDice(1, 36, true);
-		
-		/* Calling a Method */
-		printNums(5);
-		
-		/* Making an Object of type Starship */
-		StarShip s1 = new StarShip();
-		s1.name = "Enterprise";
-		s1.race = "Federation";
-		s1.speed = 5;
-		
-		s1.move();
-		
-		/* Print Using Variables and Concatenation */
-		System.out.println("Speed of " + s1.name + " is " + s1.speed + ".");
+	
 
-	}
 }
