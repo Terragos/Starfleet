7100b34294d9a903da58d738094cbe3da5251e4f
brianweese <63375668+brianweese@users.noreply.github.com>
2020-04-18 18:19:05 -0400

    Update StarShip.java

--- Starfleet/src/StarShip.java
+++ Starfleet/src/StarShip.java
@@ -1,11 +1,14 @@
 
-public class StarShip {
+public class Starship {
+	
+	public static int numShips = 0;
+	public static Starship[] starships;
 
-	String name;    // XXX-YYY where XXX=Race and YYY=ship type
-	String race;    // not needed?  
-	int speed;      // hexes moved for entire turn
-	double spi;     // speed per impulse
-	double distrv;  // distance traveled 
-	boolean move;   // does the ship move this impulse?
+	//  Starship class
+	public String name;      // name
+	public String turnMode;  // turn mode letter - converted to # of hexes to move forward before turning during Impulse Procedure
+	public int speed;        // hexes moved for entire turn
+	public double spi;       // speed per impulse
+	public double distrv;    // distance traveled 
 
 }
