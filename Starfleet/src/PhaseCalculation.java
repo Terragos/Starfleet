1ae05e6880eb7498b9254f035ae22301f068d718
brianweese <63375668+brianweese@users.noreply.github.com>
2020-04-30 02:05:33 -0400

    Update PhaseCalculation.java

--- Starfleet/src/PhaseCalculation.java
+++ Starfleet/src/PhaseCalculation.java
@@ -98,13 +98,15 @@ public class PhaseCalculation {
 					} else if (userInput.equalsIgnoreCase("R")) {                   // Remove destroyed ship/monster
 						System.out.println();
 						ShipSetup.PrintCurrentShipsInGame();
-						System.out.print("Remove which ship? [0 to cancel] ");
+						System.out.print("Remove which ship? [RETURN to cancel] ");
 						int removeInput = -1;
-						removeInput = Driver.getNumber(0, Driver.currentGameYard.numShips);
-						System.out.println("Are you sure you want to remove: " + Driver.currentGameYard.list[removeInput-1].name);
-						String yesOrNo = Driver.getInput("YN");
-						if (yesOrNo.contentEquals("Y")) {
-							Driver.currentGameYard.removeShipFromShipyard(removeInput);
+						removeInput = Driver.getNumber(1, Driver.currentGameYard.numShips);
+						if (removeInput > 0) {
+							System.out.print("Are you sure you want to remove: " + Driver.currentGameYard.list[removeInput-1].name);
+							String yesOrNo = Driver.getInput("YN");
+							if (yesOrNo.contentEquals("Y")) {
+								Driver.currentGameYard.removeShipFromShipyard(removeInput);
+							}
 						}
 					} else if (userInput.equalsIgnoreCase("A")) {  					// Add ship/monster
 						System.out.print("Add \"Add ship code\" here.");
@@ -122,7 +124,20 @@ public class PhaseCalculation {
 			}
 		} 
 		
-		AdjustForDamageControl();
+		System.out.println();
+		System.out.print("Are any ships repairing system with Damage Control? ");
+		String yesOrNo = Driver.getInputNoCancel("YN");
+		if (yesOrNo.contentEquals("Y")) {
+			AdjustForDamageControl();
+		}
+
+		System.out.println();
+		System.out.print("Are any ships using labs for scientific research? ");
+		yesOrNo = Driver.getInputNoCancel("YN");
+		if (yesOrNo.contentEquals("Y")) {
+			WeaponsDamage.labResearchPoints (WeaponsDamage.labResearchAquired);
+		}
+	
 	}
 	
 	public static void AdjustForDamageControl() {
@@ -165,19 +180,21 @@ public class PhaseCalculation {
 		int shipNumToRepair = -1;
 
 		while (cont) {
-			System.out.print("Which ship to repair damage? [0 for cancel] ");
+			System.out.print("Which ship to repair damage? [RETURN for cancel] ");
 			shipNumToRepair = Driver.getNumber(0, Driver.currentGameYard.numStarships);
 
 			if(shipNumToRepair > 0) {
-				System.out.print("Which system to repair? [0 to cancel] ");
-				int systemToRepair = Driver.getNumber(0,24);
+				System.out.print("Which system to repair? ");
+				int systemToRepair = Driver.getNumberNoCancel(1,24);
 				
 				if (systemToRepair >= 1 && systemToRepair <= 24) {
-					System.out.print("How many of that system to repair? [0 to cancel] ");
-					int quantity = Driver.getNumber(0,10);
+					System.out.print("How many of that system to repair? ");
+					int quantity = Driver.getNumberNoCancel(0,10);
 
-					Driver.currentGameYard.list[shipNumToRepair-1].ssd[systemToRepair-1].remaining = Driver.currentGameYard.list[shipNumToRepair-1].ssd[systemToRepair-1].remaining + quantity;
-					System.out.println();
+					if (quantity >= 0) {
+						Driver.currentGameYard.list[shipNumToRepair-1].ssd[systemToRepair-1].remaining = Driver.currentGameYard.list[shipNumToRepair-1].ssd[systemToRepair-1].remaining + quantity;
+						System.out.println();
+					}
 					
 					if(Driver.TESTING) {
 						System.out.println();
@@ -188,7 +205,7 @@ public class PhaseCalculation {
 					}
 				}
 			}
-			if (shipNumToRepair == 0) {
+			if (shipNumToRepair == -1) {
 				cont = false;
 			}
 		}
@@ -202,7 +219,7 @@ public class PhaseCalculation {
 			}
 			System.out.print("====");
 		}
-		System.out.println("=================");
+		System.out.println("=======================");
 		
 		int shipTotal = 0;
 		for (int i = 0; i <= Driver.currentGameYard.numShips - 1; i++) {
@@ -221,7 +238,7 @@ public class PhaseCalculation {
 			}
 			System.out.print("----");
 		}
-		System.out.println("-----------------");
+		System.out.println("-----------------------");
 
 		System.out.print("Ship Name:     ");
 	 	for (int i = 0; i <= Driver.currentGameYard.numShips - 1; i++) {
@@ -247,7 +264,7 @@ public class PhaseCalculation {
 			}
 			System.out.print("====");
 		}
-		System.out.println("=====================");
+		System.out.println("===========================");
 		
 		for (int i = 1; i <= ((18 + shipTotal)-29)/2; i++) {     //  CENTERS TEXT
 			System.out.print(" ");
@@ -267,7 +284,7 @@ public class PhaseCalculation {
 			}
 			System.out.print("====");
 		}
-		System.out.print("=================");
+		System.out.print("=======================");
 	}
 	
 	
@@ -453,42 +470,49 @@ public class PhaseCalculation {
 		System.out.println("|  (4)     IV        8          3         24          6          2        0    |");
 		System.out.println("|  (5)     I        12          2         24          6          2       1/2   |");
 		System.out.println("|==============================================================================|");
-		System.out.print("Drone Type : ");
+		System.out.print("Drone Type [RETURN to cancel]: ");
 		int droneType = Driver.getNumber(0, 5);
 		int droneSpeed = 0;
-		if (droneType == 1 || droneType == 4) {
-			System.out.print("Drone Speed: 8");
-			droneSpeed = 8;
-		} else {
-			System.out.print("Drone Speed: 12");
-			droneSpeed = 12;
+
+		if (droneType > -1) {
+			if (droneType == 1 || droneType == 4) {
+				System.out.print("Drone Speed: 8");
+				droneSpeed = 8;
+			} else {
+				System.out.print("Drone Speed: 12");
+				droneSpeed = 12;
+			}
+			System.out.println();
+			Starship drone = new Starship(Starship.Ship.DRONE, droneSpeed, droneName);
+			Driver.currentGameYard.addShipToShipyard(drone);
 		}
-		System.out.println();
-		Starship drone = new Starship(Starship.Ship.DRONE, droneSpeed, droneName);
-		Driver.currentGameYard.addShipToShipyard(drone);
 	}
 
 	// ADD SHUTTLE METHOD - ON-THE-FLY	
 	public static void AddShuttle() {	
-		System.out.print("Shuttle Name : ");
+		System.out.print("Shuttle Name [RETURN to cancel]: ");
 		String shuttleName = keyboard.nextLine();
-		
-		System.out.print("Shuttle Speed: ");
-		int shuttleSpeed = Driver.getNumber(-1, 6);
-		
-		Starship shuttle = new Starship(Starship.Ship.SHUTTLE, shuttleSpeed, shuttleName);
-		Driver.currentGameYard.addShipToShipyard(shuttle);
+
+		if (shuttleName.length() > 0) {
+			System.out.print("Shuttle Speed: ");
+			int shuttleSpeed = Driver.getNumber(-1, 6);
+			
+			Starship shuttle = new Starship(Starship.Ship.SHUTTLE, shuttleSpeed, shuttleName);
+			Driver.currentGameYard.addShipToShipyard(shuttle);
+		}
 	}
 
 	// ADD FIGHTER METHOD - ON-THE-FLY
 	public static void AddFighter() {		
-		System.out.print("Fighter Name : ");
+		System.out.print("Fighter Name [RETURN to cancel]: ");
 		String fighterName = keyboard.nextLine();
 		
-		System.out.print("Fighter Speed: ");
-		int fighterSpeed = Driver.getNumber(-1, 32);
-		
-		Starship fighter = new Starship(Starship.Ship.FIGHTER, fighterSpeed, fighterName);
-		Driver.currentGameYard.addShipToShipyard(fighter);
+		if (fighterName.length() > 0) {
+			System.out.print("Fighter Speed: ");
+			int fighterSpeed = Driver.getNumber(-1, 32);
+			
+			Starship fighter = new Starship(Starship.Ship.FIGHTER, fighterSpeed, fighterName);
+			Driver.currentGameYard.addShipToShipyard(fighter);
+		}
 	}	
 }
