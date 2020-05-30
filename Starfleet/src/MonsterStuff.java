dfcc93549379c137aa65cb2987da674713255e36
brianweese <63375668+brianweese@users.noreply.github.com>
2020-05-29 22:31:12 -0400

    Update MonsterStuff.java

--- Starfleet/src/MonsterStuff.java
+++ Starfleet/src/MonsterStuff.java
@@ -6,7 +6,7 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
 public class MonsterStuff {
 
 	public static Starship currentMonster;
-	public static Starship currentPLanet;
+	public static Starship currentPlanet;
 
 	public static int labResearch[][] = {{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10},   //  distance
 										 {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0},   //  die roll 1 
